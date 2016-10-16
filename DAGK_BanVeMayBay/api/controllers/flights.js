a268baae3d5780f24bdc3dc330c4945029e646df
Quach Ha Chan Thanh <qhcthanh@gmail.com>
2016-10-16 18:18:15 +0700

    POST đặt chỗ

--- DAGK_BanVeMayBay/api/controllers/flights.js
+++ DAGK_BanVeMayBay/api/controllers/flights.js
@@ -1,6 +1,7 @@
 
 'user strict'
 
+var util = require("util");
 var fs = require("fs");
 var mysql = require("mysql");
 var dateformat = require("dateformat");
@@ -25,7 +26,6 @@ connection.connect();
 
 // Tìm kiếm chuyến bay
 function getFlight(req,res,next) {
-	console.log("Call");
 
 	// Required
 	var fromID = req.swagger.params.masanbaydi.value;
@@ -176,6 +176,74 @@ function getFlightInfo(req,res,next) {
 // BOOK vé với POST trả về mã đặt chỗ
 function bookTicket(res,req,next) {
 
+	// Required
+	var flightID = req.swagger.params.machuyenbay.value;
+	var fromDateTimeStamp = req.swagger.params.ngaydi.value;
+	var mucgia = req.swagger.params.muc.value;
+	var hang = req.swagger.params.hang.value;
+	var soghe = req.swagger.params.soghe.value; 
+
+	var fromDate = dateformat(new Date(fromDateTimeStamp * 1000), "yyyy-mm-dd");
+
+	
+	var queryGiaVe = util.format("select giaban from chuyenbay where machuyenbay = \'%s\' and ngaydi = \'%s\' and hang = \'%s\' and muc = \'%s\'", "CF108","2016/10/20","C","F");
+
+	connection.query(queryGiaVe, function(err, results) {
+		if (!err) {
+			if (results != undefined ) { 
+				if (results.length == 0) {
+					res.sendStatus(404);
+				} else {
+					console.log(results);
+					var madatcho = generateUUID.generate(6);
+					var thoigiancapphat = dateformat(new Date(), "yyyy-mm-dd");
+					var queryDatCho = util.format("INSERT INTO DATCHO(madatcho, thoigiandatcho, tongtien, trangthai) values(\'%s\',\'%s\',%d,%d)", madatcho, thoigiancapphat , results[0].giaban * soghe, 0);
+					
+					var queryChiTietChuyenBay = util.format("INSERT INTO chitietchuyenbay(madatcho, machuyenbay, ngay, hang, mucgia) values(\'%s\',\'%s\',\'%s\',\'%s\',\'%s\')",madatcho,flightID,fromDate,hang,mucgia);
+
+					console.log(queryDatCho);
+					console.log(queryChiTietChuyenBay);
+
+					connection.query(queryDatCho, function(err,results) {
+
+						if(!err) {
+							con
+							connection.query(queryChiTietChuyenBay, function(err,results) {
+								if(!err) {
+									res.send({
+										"datcho" : {
+											"madatcho": madatcho,
+											"machuyenbay": machuyenbay,
+											"ngaydi": fromDate,
+											"thoigiancapphat": thoigiancapphat,
+											"thoihansudung": 5*60*1000
+										}
+									});
+								} else {
+									res.sendStatus(404);
+									var queryDeleteDatCho = util.format("DELETE FROM datcho where madatcho = \'%s\'",madatcho);
+									console.log(queryDeleteDatCho); 
+
+									connection.query(queryDeleteDatCho, function(err,results) {
+										if (!err) {
+											console.log(err);
+										}
+									});
+								}
+							});
+						} else {
+							res.sendStatus(404);
+						}
+					});
+				}
+			}
+		} else {
+			res.sendStatus(404);
+			console.log(err);
+		}
+	});
 }
+
+
 
 
